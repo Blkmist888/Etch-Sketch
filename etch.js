c12686aa2f13b941798f3c19798168e4d39cd842
Sheldon Johnson <blacknalljermaine7@gmail.com>
2024-03-06 08:38:00 -0600

    Configuring Grid.

--- etch.js
+++ etch.js
@@ -1,8 +1,6 @@
-function createGrid(){
-const container = document.getElementById('containerDiv')
-for (let index = 0; index < 16; index++) {
-  const divCreate= document.createElement('div')
-   console.log("divs Created (:")
-   container.appendChild(divCreate)
-}
+const num = 16
+for (let index = 0; index <=16; index++) {
+ const result= num*index
+ console.log(result)
+    
 }
