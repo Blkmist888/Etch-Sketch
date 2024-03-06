ce7d014729810cd8be5880c6809c100e36a27cc6
Sheldon Johnson <blacknalljermaine7@gmail.com>
2024-03-06 14:18:28 -0600

    Width and Height adjusted

--- etch.js
+++ etch.js
@@ -1,6 +1,25 @@
-const num = 16
-for (let index = 0; index <=16; index++) {
- const result= num*index
- console.log(result)
-    
+
+
+
+const container= document.getElementById('container')
+container.style.display='flex'
+container.style.flexWrap='wrap'
+container.style.backgroundColor='blue'
+container.style.height='512px'
+container.style.width='512px'
+
+
+const rows= 16
+const columns= 16
+
+for (let index = 0; index < rows*columns; index++) {
+ const createGrid= document.createElement('div')
+createGrid.style.boxSizing='border-box'
+createGrid.style.backgroundColor='white'
+createGrid.style.height=`32px`
+createGrid.style.width=`32px`
+createGrid.style.border=' 1px solid black'
+container.appendChild(createGrid)
+createGrid.classList.add
 }
+
